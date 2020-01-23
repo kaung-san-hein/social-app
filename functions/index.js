c8ab75be53ab520e0b42468a891a27fdee24df1f
kaung-san-hein <kaungsanhein2019@gmail.com>
2020-01-23 21:08:17 +0630

    finish 2 user registration

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,27 @@
 const functions = require("firebase-functions");
+const app = require("express")();
+
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-const express = require("express");
-const app = express();
+const firebaseConfig = {
+  apiKey: "AIzaSyCUMEKLjcdn8JGkKZEEoCI010QtzLWqB9Y",
+  authDomain: "social-app-4ed33.firebaseapp.com",
+  databaseURL: "https://social-app-4ed33.firebaseio.com",
+  projectId: "social-app-4ed33",
+  storageBucket: "social-app-4ed33.appspot.com",
+  messagingSenderId: "306321905608",
+  appId: "1:306321905608:web:b5b57833fd5be2c96712fd",
+  measurementId: "G-FSQMYETWCF"
+};
+
+const firebase = require("firebase");
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
 
 app.get("/screams", (req, res) => {
-  admin
-    .firestore()
-    .collection("screams")
+  db.collection("screams")
     .orderBy("createdAt", "desc")
     .get()
     .then(data => {
@@ -33,9 +46,7 @@ app.post("/scream", (req, res) => {
     createdAt: new Date().toISOString()
   };
 
-  admin
-    .firestore()
-    .collection("screams")
+  db.collection("screams")
     .add(newScream)
     .then(doc => {
       res.json({ message: `document ${doc.id} created successfully` });
@@ -43,6 +54,48 @@ app.post("/scream", (req, res) => {
     .catch(err => {
       res.status(500).json({ error: "something went wrong" });
       console.error(err);
+    });
+});
+
+//user registration
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
+  };
+
+  // TODO validate data
+
+  let token, userId;
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then(idToken => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch(err => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already used" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
     });
 });
 
